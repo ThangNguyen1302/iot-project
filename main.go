fe895f8b7071d0feaea131dc57bf4ee4ddecd577
Phuc Hung <nphung.htkhang2020@gmail.com>
2025-03-06 22:23:06 +0700

    Phuc Hung: add Mongo

--- main.go
+++ main.go
@@ -1,21 +1,46 @@
 package main
 
 import (
-    "encoding/json"
-    "fmt"
-    "log"
-    "net/http"
-    "my_iot_project/utils"
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"my_iot_project/utils"
+	"net/http"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // Define FeedData struct
 type FeedData struct {
-    ID        string `json:"id"`
-    Value     string `json:"value"`
-    CreatedAt string `json:"created_at"`
+    ID        string `json:"id" bson:"id"`
+    Value     string `json:"value" bson:"value"`
+    CreatedAt string `json:"created_at" bson:"created_at"`
 }
 
+var mongoClient *mongo.Client
+var mongoCollection *mongo.Collection
 
+func connectToMongoDB() {
+    var err error
+    mongoClient, err = mongo.NewClient(options.Client().ApplyURI("mongodb://localhost:27017"))
+    if err != nil {
+        log.Fatal("Error creating MongoDB client:", err)
+    }
+
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    err = mongoClient.Connect(ctx)
+    if err != nil {
+        log.Fatal("Error connecting to MongoDB:", err)
+    }
+
+    mongoCollection = mongoClient.Database("iot_data").Collection("feed_data")
+    fmt.Println("Connected to MongoDB!")
+}
 // Fetch data from Adafruit IO
 func fetchData(config util.Config) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
@@ -39,6 +64,24 @@ func fetchData(config util.Config) http.HandlerFunc {
             return
         }
 
+        // Store data into MongoDB
+        ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+        defer cancel()
+
+        // Convert slice of structs into slice of interface{} to insert into MongoDB
+        var documents []interface{}
+        for _, d := range data {
+            documents = append(documents, d)
+        }
+
+        if len(documents) > 0 {
+            _, err := mongoCollection.InsertMany(ctx, documents)
+            if err != nil {
+                log.Println("Error inserting data into MongoDB:", err)
+            } else {
+                fmt.Println("Data successfully stored in MongoDB!")
+            }
+        }
         // Enable CORS so frontend can access this API
         w.Header().Set("Access-Control-Allow-Origin", "*")
         w.Header().Set("Content-Type", "application/json")
@@ -49,6 +92,7 @@ func fetchData(config util.Config) http.HandlerFunc {
 }
 
 func main() {
+    connectToMongoDB()
     config, err := util.LoadConfig(".")
     if err != nil {
         log.Fatal("Failed to load config:", err)
